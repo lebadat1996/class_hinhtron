848be55717e3af1791ed1f896cdee8c6067ed34f
badat <you@example.com>
2020-03-03 15:54:31 +0700

    circle

---
+++ app.js
@@ -0,0 +1,18 @@
+let Circle = function (radius) {
+    this.radius = radius;
+
+    this.getRadius = function () {
+        return radius;
+    };
+    this.getArea = function () {
+        return Math.PI * radius * radius;
+    };
+};
+let circle = new Circle(2);
+let radius = circle.getRadius();
+let area = circle.getArea();
+alert("radius: " + radius + "; area: " + area);
+let circle1 = new Circle(5);
+let radius1 = circle1.getRadius();
+let area1 = circle1.getArea();
+alert("radius: " + radius1 + "; area: " + area1);
